9a01165e507932e59026173248bfed087f2e7e88
evan <evan.yu6748@gmail.com>
2025-02-08 17:46:36 -0500

    dittoo

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase
-service_account_info = json.loads(os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"))
+service_account_info = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 
 
 app = FastAPI()
+print(os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"))
 
 @app.post("/book")
 async def book_desk(data: dict):
     # Process booking logic here
     user_id = data.get("user_id")
     resource_id = data.get("resource_id")
-    db.collection("bookings").add(data)
+    cred.collection("bookings").add(data)
     return {"message": "Booking request received"}
