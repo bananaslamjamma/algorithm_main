f4fbbb3b08bad12eadc3b93ec1886b5b7e73c7ed
evan <evan.yu6748@gmail.com>
2025-02-08 18:43:56 -0500

    creds

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-import os
 import json
-import logging
-from fastapi import FastAPI, HTTPException
-from google.cloud import firestore
+import os
+from fastapi import FastAPI
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
+import logging
+from fastapi import HTTPException
 
-# Load credentials from environment variable
+# Initialize Firebase
 service_account_info = json.loads(os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"))
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 
+
 app = FastAPI()
+print(os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"))
 
 logging.basicConfig(level=logging.INFO)
-
-# Initialize Firestore
-db = firestore.Client()
+# Init Firestore
+db = firestore.client(credentials=cred)
 
 @app.post("/book")
 async def book_desk(data: dict):
     try:
-        user_id = data.get("user_id")
+        user_id = 1
         resource_id = data.get("resource_id")
         if not user_id or not resource_id:
             raise HTTPException(status_code=400, detail="Missing user_id or resource_id")
@@ -34,10 +35,9 @@ async def book_desk(data: dict):
             "name": data.get("name", "")
         }
         
-        doc_ref = db.collection("bookings").document(f"booking_{user_id}")
+        doc_ref = cred.collection("bookings").document(f"booking_1")
         doc_ref.set(booking_data)  # Using set() to overwrite any existing document with the same ID
         
         return {"message": "Booking request received"}
     except Exception as e:
-        logging.error(f"Error occurred: {e}")
         raise HTTPException(status_code=500, detail=str(e))
